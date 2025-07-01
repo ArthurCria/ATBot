fa2d04d1e77cd7d36bbf19bb474ac6b2e03ce83e
ArthurCria <arthurwilltutui@gmail.com>
2025-06-30 21:36:35 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -5,17 +5,11 @@ idade_ATBot = 3
 if idade > idade_ATBot:
     print(f"Você é mais velho que eu! Tenho {idade_ATBot} anos!")
 elif idade < idade_ATBot:
-     print(f"Uau! Você é mais novo que eu! Tenho {idade_ATBot} anos!")
+    print(f"Uau! Você é mais novo que eu! Tenho {idade_ATBot} anos!")
 else:
-     print("Sua idade é igual a minha!!")
-questao = int(input("Agora uma pergunta de matemática! Quanto é 24 + 34? "))
-soma = 24 + 34
-if questao == soma:
-    print("Você acertou! Muito bem!")
-else:
-    print("Você errou. Mas tudo bem!")
+    print("Sua idade é igual a minha!!")
 while True:
-    horario = int(input("Digite a hora de onde você esta(de 0 a 23): "))
+    horario = int(input("Digite a hora de onde você está(de 0 a 23): "))
     if 5 <= horario <= 12:
         print("Bom dia pra você!")
         break
@@ -27,4 +21,15 @@ while True:
         break
     else:
         print("Horário inválido! Digite números de 0 até 23!")
-print("Obrigado por conversar comigo! Foi bom conhecer você!")
+questao = int(input("Agora uma pergunta de matemática! Quanto é 24 + 34? "))
+soma = 24 + 34
+if questao == soma:
+    print("Você acertou! Muito bem!")
+else:
+    print("Você errou. Mas tudo bem!")
+print("Vou fazer uma charada! Dica! é uma fruta amarela!")
+charada = input("O que é o que é, tem coroa mais não é rei?: ")
+if charada == "abacaxi" or charada == "Abacaxi" or charada == "ABACAXI":
+    print(f"Muito bem {nome}! Você acertou!")
+else:
+    print("Você errou, a resposta era abacaxi")
